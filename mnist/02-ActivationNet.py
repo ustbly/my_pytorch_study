20af8b1f7f3e9b5f66a8786499b6241fca903889
ustbly <ustbly2021@126.com>
2023-05-04 12:56:30 +0800

    update

---
+++ mnist/02-ActivationNet.py
@@ -0,0 +1,105 @@
+import torch
+from torch import nn, optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+from model import ActivationNet
+
+if __name__ == '__main__':
+    # Hyperparameters
+    batch_size = 64
+    learning_rate = 1e-2
+    num_epochs = 20
+
+    # 数据预处理的组合操作
+    data_tf = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize([0.5], [0.5])
+        ]
+    )
+
+    # 下载训练数据集
+    train_dataset = datasets.MNIST(root='./data', train=True, transform=data_tf, download=True)
+    test_dataset = datasets.MNIST(root='./data', train=False, transform=data_tf)
+
+    # 解决pytorch训练的时候GPU利用率不能跑满的情况：
+    '''
+        1. DataLoader添加num_workers参数增加多个进程（此参数在Windows上打开必须将代码放到main函数才能正常执行）
+        2. DataLoader添加pin_memory参数，省掉了将数据从CPU传入到缓存RAM里面，再给传输到GPU上；为True时是直接映射到GPU的相关内存块上
+        
+    '''
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=16, pin_memory=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    model = ActivationNet(28 * 28, 300, 100, 10)
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(device)
+    model = model.to(device)
+    criterion = nn.CrossEntropyLoss().to(device)
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+
+    # 训练网络
+    model.train()
+    for epoch in range(1, num_epochs + 1):
+        total_loss = 0
+        for i, (inputs, labels) in enumerate(train_loader):
+            # 将数据也移动到GPU上
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            inputs = inputs.view(inputs.size(0), -1)
+            optimizer.zero_grad()
+            output = model(inputs)
+            loss = criterion(output, labels)
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+
+            if i % 100 == 0:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch, i * len(inputs), len(train_loader.dataset),
+                           100. * i / len(train_loader), loss.item()))
+
+        avg_loss = total_loss / len(train_loader)
+        print('Epoch: {} Average loss: {:.4f}'.format(epoch, avg_loss))
+    # # 测试模型
+    # model.eval()
+    # eval_acc = 0.0
+    # eval_loss = 0.0
+    # for inputs, labels in test_loader:
+    #     # 将数据也移动到GPU上
+    #     inputs = inputs.to(device)
+    #     labels = labels.to(device)
+    #
+    #     inputs = inputs.view(inputs.size(0), -1)
+    #     optimizer.zero_grad()
+    #     outputs = model(inputs)
+    #     loss = criterion(outputs, labels)
+    #     optimizer.step()
+    #
+    #     eval_loss += loss.item() * labels.size(0)
+    #     _, pred = torch.max(outputs, 1)
+    #     num_correct = (pred == labels).sum()
+    #     eval_acc += num_correct.item()
+    # print(f"loss: {eval_loss / len(test_dataset)},acc: {eval_acc / len(test_dataset)}")
+    # 在测试集上进行测试
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for inputs, labels in test_loader:
+            # 将数据也移动到GPU上
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            inputs = inputs.view(inputs.size(0), -1)
+            output = model(inputs)
+            test_loss += criterion(output, labels).sum().item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(labels.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    accuracy = 100. * correct / len(test_loader.dataset)
+    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
+        test_loss, correct, len(test_loader.dataset), accuracy))
---
+++ mnist/model.py
@@ -0,0 +1,110 @@
+from torch import nn
+
+
+class simpleNet(nn.Module):
+    def __init__(self, in_dim, n_hidden_1, n_hidden_2, out_dim):
+        super(simpleNet, self).__init__()
+        self.layer1 = nn.Linear(in_dim, n_hidden_1)
+        self.layer2 = nn.Linear(n_hidden_1, n_hidden_2)
+        self.layer3 = nn.Linear(n_hidden_2, out_dim)
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        return x
+
+
+# 添加激活函数ReLU
+class ActivationNet(nn.Module):
+    def __init__(self, in_dim, n_hidden_1, n_hidden_2, out_dim):
+        super(ActivationNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Linear(in_dim, n_hidden_1),
+            nn.ReLU(True)
+        )
+        self.layer2 = nn.Sequential(
+            nn.Linear(n_hidden_1, n_hidden_2),
+            nn.ReLU(True)
+        )
+        self.layer3 = nn.Sequential(
+            nn.Linear(n_hidden_2, out_dim),
+            nn.ReLU(True)
+        )
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        return x
+
+
+# 添加批标准化
+class BatchNet(nn.Module):
+    def __init__(self, in_dim, n_hidden_1, n_hidden_2, out_dim):
+        super(BatchNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Linear(in_dim, n_hidden_1),
+            nn.BatchNorm1d(n_hidden_1),
+            nn.ReLU()
+        )
+        self.layer2 = nn.Sequential(
+            nn.Linear(n_hidden_1, n_hidden_2),
+            nn.BatchNorm1d(n_hidden_2),
+            nn.ReLU()
+        )
+        self.layer3 = nn.Sequential(
+            nn.Linear(n_hidden_2, out_dim),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        return x
+
+
+# 卷积神经网络
+class CNN(nn.Module):
+    def __init__(self, in_dim, n_hidden_1, n_hidden_2, out_dim):
+        super(CNN, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=3),
+            nn.BatchNorm2d(16),
+            nn.ReLU(inplace=True)
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=3),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(128 * 4 * 4, 1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(1024, 128),
+            nn.ReLU(inplace=True),
+            nn.Linear(128, 10)
+        )
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
