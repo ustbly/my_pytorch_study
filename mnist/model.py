01aa674075267fe99a42c732fcb939a0a9375294
ustbly <ustbly2021@126.com>
2023-05-04 13:17:30 +0800

    update

--- mnist/model.py
+++ mnist/model.py
@@ -70,26 +70,26 @@ class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=3),
+            nn.Conv2d(1, 16, kernel_size=3),  # (batch,16,26,26)
             nn.BatchNorm2d(16),
             nn.ReLU(inplace=True)
         )
         self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=3),
+            nn.Conv2d(16, 32, kernel_size=3),  # (batch,32,24,24)
             nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2)
+            nn.MaxPool2d(kernel_size=2, stride=2)   # (batch,32,12,12)
         )
         self.layer3 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=3),
+            nn.Conv2d(32, 64, kernel_size=3),   # (batch,64,10,10)
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True)
         )
         self.layer4 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=3),
+            nn.Conv2d(64, 128, kernel_size=3),  # (batch,128,8,8)
             nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2)
+            nn.MaxPool2d(kernel_size=2, stride=2)   # (batch,128,4,4)
         )
 
         self.fc = nn.Sequential(
